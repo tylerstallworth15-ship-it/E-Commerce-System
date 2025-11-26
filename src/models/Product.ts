daa9023b3b677dcd9369d0c7d8ce0a4ea2f3ef93
Tyler Glen <tylerstallworth15@gmail.com>
2025-11-25 20:24:45 -0500

    Products

---
+++ src/models/Product.ts
@@ -0,0 +1,40 @@
+export class Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    discountPercentage: number;
+    category: string;
+
+    constructor(
+        id: number,
+        name: string,
+        price: number,
+        description: string,
+        discountPercentage: number,
+        category: string,
+    ) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.discountPercentage = discountPercentage;
+        this.category = category;
+    }
+
+    displayDetails(): string{
+        const displayItem =
+        "Product: " + this.name +
+        " | Price: $" + this.price +
+        " | Discount: " + this.discountPercentage +
+        " | Category: " + this.category +
+        " | Description: " + this.description;
+        return displayItem;
+    }
+
+    getPricesWithDiscount(): number {
+        const discountAmount = (this.price * this.discountPercentage) / 100;
+        const finalPrice = this.price - discountAmount;
+        return finalPrice; 
+    }
+}
---
+++ src/services/apiService.ts
@@ -0,0 +1,8 @@
+export async function getAllProducts(): Promise<any[]> {
+    const response = await fetch("https://dummyjson.com/products");
+    if (!response.ok) {
+        throw new Error("Could not get products");
+    }
+    const data = await response.json();
+    return data.products;
+}
