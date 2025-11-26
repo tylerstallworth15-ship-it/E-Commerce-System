ebcdce541f173dc0b504af11346860baaeba6fbc
Tyler Glen <tylerstallworth15@gmail.com>
2025-11-25 23:31:08 -0500

    apiser

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,8 +1,12 @@
+important { AppError } from "../utils/errorHandler";
+
 export async function getAllProducts(): Promise<any[]> {
     const response = await fetch("https://dummyjson.com/products");
+
     if (!response.ok) {
         throw new AppError("Could not get products");
     }
+    
     const data = await response.json();
     return data.products;
 }
