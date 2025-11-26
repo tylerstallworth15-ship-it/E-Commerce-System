1284ef520219768228d423bccfdee753a876cab2
Tyler Glen <tylerstallworth15@gmail.com>
2025-11-25 23:42:07 -0500

    complete

--- dist/services/apiService.js
+++ dist/services/apiService.js
@@ -1,16 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAllProducts = getAllProducts;
-important;
-{
-    AppError;
-}
-from;
-"../utils/errorHandler";
+const errorHandler_1 = require("../utils/errorHandler");
 async function getAllProducts() {
     const response = await fetch("https://dummyjson.com/products");
     if (!response.ok) {
-        throw new AppError("Could not get products");
+        throw new errorHandler_1.AppError("Could not get products");
     }
     const data = await response.json();
     return data.products;
